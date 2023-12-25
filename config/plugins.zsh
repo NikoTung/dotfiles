32e7b6fc18bca733f4172793ea8f07ca6c25481a
niko <n@eiko.me>
2023-12-25 13:25:56 +0800

    bind key for history-substring-search

--- config/plugins.zsh
+++ config/plugins.zsh
@@ -6,12 +6,15 @@ zplug "plugins/sudo", from:oh-my-zsh
 zplug "plugins/command-not-found", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting"
 zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-history-substring-search", as:plugin
 zplug "zsh-users/zsh-completions"
 #zplug "plugins/tig", from:oh-my-zsh
 zplug "MenkeTechnologies/zsh-tig-plugin"
 zplug "agkozak/zsh-z"
 zplug "z-shell/zsh-eza"
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
 
 # zplug - install/load new plugins when zsh is started or reloaded
 if ! zplug check --verbose; then
